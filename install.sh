610386f148462d71932b19043fa33bceb70d7287
KVTolkachev <KVTolkachev@alfabank.ru>
2014-11-03 17:14:00 +0300

    First commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+cd $(dirname $BASH_SOURCE)
+
+ln -sf ./home/.oh-my-zsh/custom/themes/lavcraft.zsh-theme ~/.oh-my-zsh/custom/themes/lavcraft.zsh-theme
+ln -sf ./home/.zshrc ~/.zshrc
+ln -sf ./home/.gitconfig ~/.gitconfig
